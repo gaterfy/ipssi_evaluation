34ba267728fd67149d781145cd0ccd5024d6ed7c
kunsangabo ndongala <berfy@simpleandnew.com>
2019-06-28 12:16:22 +0200

    add jshint analyser

--- src/generator.js
+++ src/generator.js
@@ -1,9 +1,7 @@
 function createBitcoin() {
-    console.log('Loading ...')
-
+    console.log('Loading ...');
     setTimeout(() => console.log('Created !'), 1000);
-
-    let varToRemove = true;
+    //let varToRemove = true; jamais utilisé
 }
 
 module.exports = {
